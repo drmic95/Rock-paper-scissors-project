85c81e1b50d2ad3e5c75fa2fbd3f4ca4998a0054
drmic95 <101579862+drmic95@users.noreply.github.com>
2022-04-29 14:12:40 +0200

    Add files via upload
    first version of functioning code

---
+++ app.js
@@ -0,0 +1,31 @@
+const options = ['rock', 'paper', 'scissors'];
+
+function computerPlay(random) {
+  let random1 = Math.floor(Math.random() * random.length);
+  return random1;
+}
+const playerSelection = 'rock';
+let computerSelection = computerPlay(options);
+
+function playRound(playerSelection, computerSelection) {
+  if (computerSelection == 0 && playerSelection == 'rock') {
+    console.log('tie');
+  } else if (computerSelection == 0 && playerSelection == 'paper') {
+    console.log('player wins');
+  } else if (computerSelection == 0 && playerSelection == 'scissors') {
+    console.log('computer wins');
+  } else if (computerSelection == 1 && playerSelection == 'rock') {
+    console.log('computer wins');
+  } else if (computerSelection == 1 && playerSelection == 'paper') {
+    console.log('tie');
+  } else if (computerSelection == 1 && playerSelection == 'scissors') {
+    console.log('player wins');
+  } else if (computerSelection == 2 && playerSelection == 'rock') {
+    console.log('player wins');
+  } else if (computerSelection == 2 && playerSelection == 'paper') {
+    console.log('computer wins');
+  } else if (computerSelection == 2 && playerSelection == 'scissors') {
+    console.log('tie');
+  }
+}
+playRound(playerSelection, computerSelection);
