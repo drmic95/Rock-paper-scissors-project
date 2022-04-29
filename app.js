08c9f7419af5c5e93cc96f049f1e91c7eccc648f
drmic95 <ante.drmic1b@gmail.com>
2022-04-29 16:08:36 +0200

    scores for only one round

--- app.js
+++ app.js
@@ -7,25 +7,37 @@ function computerPlay(random) {
 const playerSelection = prompt('');
 let computerSelection = computerPlay(options);
 
+let playerScoreTotal = 0;
+let compScoreTotal = 0;
+
 function playRound(playerSelection, computerSelection) {
   if (computerSelection == 0 && playerSelection == 'rock') {
     console.log('tie');
   } else if (computerSelection == 0 && playerSelection == 'paper') {
     console.log('player wins');
+    return (playerScoreTotal = playerScoreTotal + 1);
   } else if (computerSelection == 0 && playerSelection == 'scissors') {
     console.log('computer wins');
+    return (compScoreTotal = compScoreTotal + 1);
   } else if (computerSelection == 1 && playerSelection == 'rock') {
     console.log('computer wins');
+    return (compScoreTotal = compScoreTotal + 1);
   } else if (computerSelection == 1 && playerSelection == 'paper') {
     console.log('tie');
   } else if (computerSelection == 1 && playerSelection == 'scissors') {
     console.log('player wins');
+    return (playerScoreTotal = playerScoreTotal + 1);
   } else if (computerSelection == 2 && playerSelection == 'rock') {
     console.log('player wins');
+    return (playerScoreTotal = playerScoreTotal + 1);
   } else if (computerSelection == 2 && playerSelection == 'paper') {
     console.log('computer wins');
+    return (compScoreTotal = compScoreTotal + 1);
   } else if (computerSelection == 2 && playerSelection == 'scissors') {
     console.log('tie');
   }
 }
 playRound(playerSelection, computerSelection);
+
+console.log('playerScore', playerScoreTotal);
+console.log('computerScore', compScoreTotal);
