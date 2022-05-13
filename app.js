ccb4fdaafcbc445d3fa83f6a4ed300fd1f37b759
drmic95 <ante.drmic1b@gmail.com>
2022-05-13 10:21:55 +0200

    cleaner code, loop unresolved

--- app.js
+++ app.js
@@ -12,47 +12,32 @@ let computerSelection = computerPlay(options);
 let playerScoreTotal = 0;
 let compScoreTotal = 0;
 
-function playRound(playerSelection, computerSelection) {
-  if (computerSelection == 0 && playerSelection == 'rock') {
-    console.log('tie');
-  } else if (computerSelection == 0 && playerSelection == 'paper') {
-    console.log('player wins');
-    return (playerScoreTotal = playerScoreTotal + 1);
-  } else if (computerSelection == 0 && playerSelection == 'scissors') {
-    console.log('computer wins');
-    return (compScoreTotal = compScoreTotal + 1);
-  } else if (computerSelection == 1 && playerSelection == 'rock') {
-    console.log('computer wins');
-    return (compScoreTotal = compScoreTotal + 1);
-  } else if (computerSelection == 1 && playerSelection == 'paper') {
-    console.log('tie');
-  } else if (computerSelection == 1 && playerSelection == 'scissors') {
-    console.log('player wins');
-    return (playerScoreTotal = playerScoreTotal + 1);
-  } else if (computerSelection == 2 && playerSelection == 'rock') {
-    console.log('player wins');
-    return (playerScoreTotal = playerScoreTotal + 1);
-  } else if (computerSelection == 2 && playerSelection == 'paper') {
-    console.log('computer wins');
-    return (compScoreTotal = compScoreTotal + 1);
-  } else if (computerSelection == 2 && playerSelection == 'scissors') {
-    console.log('tie');
-  }
-}
-
-function game() {
+function playRound(computerSelection, playerSelection) {
   for (let i = 0; i < 10; i++) {
-    playRound(playerSelection, computerSelection)[i];
-    console.log(
-      `player score ${playerScoreTotal}...computer score ${compScoreTotal}`
-    );
+    computerSelection[i];
+    if (
+      (computerSelection === 0 && playerSelection === 'paper') ||
+      (computerSelection === 1 && playerSelection === 'scissors') ||
+      (computerSelection === 2 && playerSelection === 'rock')
+    ) {
+      console.log(`player wins`);
+      playerScoreTotal++;
+      console.log(
+        `player score ${playerScoreTotal}...computer score ${compScoreTotal}`
+      );
+    } else if (
+      (computerSelection === 0 && playerSelection === 'scissors') ||
+      (computerSelection === 1 && playerSelection === 'rock') ||
+      (computerSelection === 2 && playerSelection === 'paper')
+    ) {
+      // computer wins
+      console.log(`computer wins`);
+      compScoreTotal++;
+      console.log(
+        `player score ${playerScoreTotal}...computer score ${compScoreTotal}`
+      );
+    }
   }
 }
 
-game();
-
-// playRound(playerSelection, computerSelection);
-
-// console.log('playerScore', playerScoreTotal);
-// console.log('computerScore', compScoreTotal);
-//   `player score ${playerScoreTotal[i]}...computer score ${compScoreTotal[i]}`;
+playRound(computerSelection, playerSelection);
