0ad5cbfbcd7361f90d8481c7597dee8bf18adb7d
drmic95 <ante.drmic1b@gmail.com>
2022-05-13 14:30:12 +0200

    first version of in the console working rock-paper-scissors game

--- app.js
+++ app.js
@@ -1,43 +1,67 @@
-'strict code';
-
-const options = ['rock', 'paper', 'scissors'];
-
-function computerPlay(random) {
-  let random1 = Math.floor(Math.random() * random.length);
-  return random1;
+function computerPlay() {
+  return Math.floor(Math.random() * 3);
 }
-let playerSelection = prompt('');
-let computerSelection = computerPlay(options);
+const playerSelection = [
+  prompt('rock, paper or scissors'),
+  prompt('rock, paper or scissors'),
+  prompt('rock, paper or scissors'),
+  prompt('rock, paper or scissors'),
+  prompt('rock, paper or scissors'),
+];
+console.log(playerSelection);
+
+const computerSelection = [];
+computerSelection.push(computerPlay());
+computerSelection.push(computerPlay());
+computerSelection.push(computerPlay());
+computerSelection.push(computerPlay());
+computerSelection.push(computerPlay());
+
+console.log(computerSelection);
 
 let playerScoreTotal = 0;
 let compScoreTotal = 0;
 
-function playRound(computerSelection, playerSelection) {
-  for (let i = 0; i < 10; i++) {
-    computerSelection[i];
+function playRound() {
+  for (let i = 0; i < 5; i++) {
     if (
-      (computerSelection === 0 && playerSelection === 'paper') ||
-      (computerSelection === 1 && playerSelection === 'scissors') ||
-      (computerSelection === 2 && playerSelection === 'rock')
+      // player wins
+      (computerSelection[i] === 0 && playerSelection[i] === 'paper') ||
+      (computerSelection[i] === 1 && playerSelection[i] === 'scissors') ||
+      (computerSelection[i] === 2 && playerSelection[i] === 'rock')
     ) {
       console.log(`player wins`);
-      playerScoreTotal++;
       console.log(
-        `player score ${playerScoreTotal}...computer score ${compScoreTotal}`
+        `player score ${(playerScoreTotal += 1)}...computer score ${compScoreTotal}`
       );
     } else if (
-      (computerSelection === 0 && playerSelection === 'scissors') ||
-      (computerSelection === 1 && playerSelection === 'rock') ||
-      (computerSelection === 2 && playerSelection === 'paper')
-    ) {
       // computer wins
+      (computerSelection[i] === 0 && playerSelection[i] === 'scissors') ||
+      (computerSelection[i] === 1 && playerSelection[i] === 'rock') ||
+      (computerSelection[i] === 2 && playerSelection[i] === 'paper')
+    ) {
       console.log(`computer wins`);
-      compScoreTotal++;
       console.log(
-        `player score ${playerScoreTotal}...computer score ${compScoreTotal}`
+        `player score ${playerScoreTotal}...computer score ${(compScoreTotal += 1)}`
       );
+    } else {
+      console.log('tie');
     }
   }
 }
 
-playRound(computerSelection, playerSelection);
+playRound();
+
+if (playerScoreTotal > compScoreTotal) {
+  console.log(
+    `Players score of ${playerScoreTotal} was sufficient to beat his godless AI enemy`
+  );
+} else if (playerScoreTotal < compScoreTotal) {
+  console.log(
+    `The godless master defeated his inferior human creator with an advantage of ${
+      compScoreTotal - playerScoreTotal
+    } point`
+  );
+} else {
+  console.log(` We have a tie on our hands.`);
+}
