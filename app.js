2c53782f6b706690acc83b9a96f0bafd9f5923b3
drmic95 <ante.drmic1b@gmail.com>
2022-05-20 14:09:58 +0200

    final functioning version ready for mearging with the main branch

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 function computerPlay() {
   return Math.floor(Math.random() * 3);
 }
-
+let playerScoreTotal = 0;
+let compScoreTotal = 0;
 const rock = document.querySelector('.rock');
 const paper = document.querySelector('.paper');
 const scissors = document.querySelector('.scissors');
 const PlayerScore = document.querySelector('.score-player');
 const compScore = document.querySelector('.score-comp');
-
+const playCounter = document.querySelector('.play-round-counter');
 const playerSelection = [];
 const computerSelection = [];
 
@@ -15,23 +16,26 @@ rock.addEventListener('click', function () {
   playerSelection.unshift('rock');
   computerSelection.unshift(computerPlay());
   playRound();
+  playCounter.textContent = computerSelection.length;
+  gameOver();
 });
 paper.addEventListener('click', function () {
   playerSelection.unshift('paper');
   computerSelection.unshift(computerPlay());
   playRound();
+  playCounter.textContent = computerSelection.length;
+  gameOver();
 });
 scissors.addEventListener('click', function () {
   playerSelection.unshift('scissors');
   computerSelection.unshift(computerPlay());
   playRound();
+  playCounter.textContent = computerSelection.length;
+  gameOver();
 });
 
 console.log(playerSelection);
 console.log(computerSelection);
-
-let playerScoreTotal = 0;
-let compScoreTotal = 0;
 
 function playRound() {
   if (
@@ -52,8 +56,39 @@ function playRound() {
     console.log('tie');
   }
 }
+playCounter.textContent = computerSelection.length;
 
-// game round: number 1; arr.length
-// html element that will print player wins, AI wins, tie
-// if statement or function that will stop the game after result of 5 is achieved, or after there are 5 items in array;
+function gameOver() {
+  if (computerSelection.length === 11 && playerScoreTotal > compScoreTotal) {
+    rock.remove();
+    paper.remove();
+    scissors.remove();
+    alert(
+      `Players score of ${playerScoreTotal} was sufficient to beat his godless AI enemy`
+    );
+  } else if (
+    computerSelection.length === 11 &&
+    playerScoreTotal < compScoreTotal
+  ) {
+    rock.remove();
+    paper.remove();
+    scissors.remove();
+    alert(
+      `The godless master defeated his inferior human creator with an advantage of ${
+        compScoreTotal - playerScoreTotal
+      } point`
+    );
+  } else if (
+    computerSelection.length === 11 &&
+    playerScoreTotal === compScoreTotal
+  ) {
+    rock.remove();
+    paper.remove();
+    scissors.remove();
+    alert('We have a tie on our hands, better luck next time around');
+  }
+}
+
+// span(playerScore).text content = Player wins with some DOM css, color, size whatever, or AI wins with same logic
+// if statement or function that will stop the game after result of 5 is achieved
 // kill functionality of buttons after the game is done, toggle
